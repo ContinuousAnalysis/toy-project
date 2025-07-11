3fc44ad3406477b5dca10126fca2622752561bbd
Stephen Shen <Stephen_Shen0512@163.com>
2025-07-11 08:11:14 -0400

    Add some random tests

---
+++ tests/socket_socket_settimeout_test.py
@@ -0,0 +1,28 @@
+import socket
+
+# create a new socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def test_ok_1():
+    assert 1 == 1
+
+def test_ok_2():
+    s.settimeout(None)
+
+def test_ok_3():
+    s.settimeout(0)
+
+def test_ok_4():
+    s.settimeout(2.4)
+
+def test_violation_1():
+    try:
+        s.settimeout(-3)
+    except ValueError:
+        pass
+
+def test_violation_2():
+    try:
+        s.settimeout(-3.4)
+    except ValueError:
+        pass
